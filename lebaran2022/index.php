fad2fff1648fa4d356bea93d9aade7b41232c6d8
Brian Aji P <41229609+brianajiks123@users.noreply.github.com>
2022-05-01 13:08:39 +0700

    Add files via upload

---
+++ lebaran2022/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Idul Fitri 1443 | 2022</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="bg">
+		<img src="img/if-3.jpg">
+	</div>
+
+	<div class="jumbotron jumbotron-fluid text-center text-light bg-1">
+	  <div class="container">
+	    <h3 class="display-4">Selamat Idul Fitri</h3>
+	    <h4 class="display-4">1443 | 2022</h4>
+	    <p class="lead">Taqabbalallaahu minna wa minkum, taqabbal ya karim</p>
+	    <p class="lead">Minal ‘aidin wal faizin, <br> saya dan keluarga mohon maaf lahir dan batin.</p>
+	  </div>
+	</div>
+
+	<div class="card text-center">
+	  <div class="card-header">
+	  	<marquee>Terimakasih sudah berkunjung &#128151;</marquee>
+	  </div>
+	  <div class="card-body">
+	    <a href="mailto:brianajiks123@gmail.com?subject=Ucapan%20Idul%20Fitri&body=Selamat%20Idul%20Fitri%20.%20Mohon%20maaf%20lahir%20dan%20batin." target="_blank" class="btn btn-success">Kirim ucapan</a>
+	  </div>
+	  <div class="card-footer text-muted">
+	    &copy; Brian Aji P - <?php echo date('Y'); ?>
+	  </div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
